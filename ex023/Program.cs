bad281d541a29406c499d63b036bf954d4b29c88
lowrider48rus <lowrider48rus@gmail.com>
2022-09-25 11:51:31 +0300

    ex023

---
+++ ex023/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введи число: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<=n; i++)
+{
+    int cubn = Convert.ToInt32(Math.Pow(i,3));
+    Console.WriteLine(cubn);
+}
